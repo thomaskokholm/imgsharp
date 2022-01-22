d90848f382423b40e4666153f07101abce05b848
Thomas Kokholm <thomas@kokholm.org>
2022-01-23 00:27:41 +0100

    Implemented basic scale with resize operation

--- index.js
+++ index.js
@@ -1,11 +1,41 @@
-const express = require('express');
+const express = require("express");
 const helmet = require("helmet");
+const axios = require("axios");
+const sharp = require("sharp");
 const app = express();
 
 app.use(helmet());
 
-app.get('/', function (_req, res) {
-    res.send('Imgsharp - a high performing Node.js Image Processing Service.')
-})
+app.get("/", function (_req, res) {
+    res.send("Imgsharp - a high performing Node.js Image Processing Service.");
+});
 
-app.listen(3000)
+app.get("/tx/*", async (req, res) => {
+    try {
+        if (req.params[0]) {
+            const uri = encodeURI(req.params[0]);
+            const query = req.query;
+            console.log("tx", query, uri);
+
+            const { data } = await axios({ url: uri, responseType: "arraybuffer" });
+
+            const tx = {};
+            if (query.w) {
+                tx.width = parseInt(query.w);
+            }
+            if (query.h) {
+                tx.height = parseInt(query.h);
+            }
+            const output = await sharp(data).resize(tx).webp().toBuffer();
+
+            res.contentType("image/webp");
+            res.end(output);
+        } else {
+            res.status(500).send();
+        }
+    } catch (error) {
+        res.status(500).send();
+    }
+});
+
+app.listen(3000);
