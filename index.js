183bfb51c91096d25360259810b3dd32f8d6ee0f
Thomas Kokholm <thomas@kokholm.org>
2022-01-23 09:08:32 +0100

    Implemented aspect ratio transformation

--- index.js
+++ index.js
@@ -17,20 +17,43 @@ app.get("/tx/*", async (req, res) => {
             const query = req.query;
             console.log("tx", query, uri);
 
-            const { data } = await axios({ url: uri, responseType: "arraybuffer" });
+            axios({ url: uri, responseType: "arraybuffer" })
+                .then((arraybuffer) => {
+                    const data = arraybuffer.data;
+                    const tx = {};
+                    if (query.w) {
+                        tx.width = parseInt(query.w);
+                    }
+                    if (query.h) {
+                        tx.height = parseInt(query.h);
+                    } else if (query.aspect) {
+                        const a = query.aspect.split(":");
+                        const ratio = parseInt(a[0]) / parseInt(a[1])
+                        tx.height = Math.round(tx.width / ratio);
+                    }
 
-            const tx = {};
-            if (query.w) {
-                tx.width = parseInt(query.w);
-            }
-            if (query.h) {
-                tx.height = parseInt(query.h);
-            }
-            const output = await sharp(data).resize(tx).webp().toBuffer();
+                    sharp(data)
+                        .resize(tx)
+                        .withMetadata()
+                        .webp({ quality: 90 })
+                        .toBuffer()
+                        .then((output) => {
+                            res.contentType("image/webp");
+                            res.end(output);
+                        })
+                        .catch((sharpErr) => {
+                            console.error("sharpErr", sharpErr);
+                            res.status(500).send();
+                        });
 
-            res.contentType("image/webp");
-            res.end(output);
+
+                })
+                .catch((err) => {
+                    console.error("axiosErr", err);
+                    res.status(500).send();
+                });
         } else {
+            console.error("req.params[0] missing");
             res.status(500).send();
         }
     } catch (error) {
